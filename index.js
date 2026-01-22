c2028c921d64d490f1390252bf6d169a98814537
Martha659 <martha.blackcloud@students.msswarriors.org>
2026-01-22 20:35:29 +0000

    2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function markAsDone(todos) {
+  const modifiedTodos = [];
+
+  todos.forEach(todo => {
+    const modifiedTodo = {
+      ...todo, 
+      description: 'done - ' + todo.description
+    };
+    modifiedTodos.push(modifiedTodo);
+  });
+
+  return modifiedTodos
+
+
+
+
+
+
 
